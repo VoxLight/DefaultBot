6bfab6704639c5f2440ebba1144ffc30ea4c9ab4
VoxLight <tkkt392@gmail.com>
2021-04-01 19:18:28 -0400

    Removed unused cogs, added functionality to existing ones.

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,12 +1,17 @@
 import discord
 from discord.ext import commands
 
-
+"""
+Allows testing if online
+"""
 class Basic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         print("Basic cog loaded.")
-
+        
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"Ping: {self.bot.latency}ms")
 
 def setup(bot):
     bot.add_cog(Basic(bot))
